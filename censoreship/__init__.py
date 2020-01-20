2579e38a43ff25ed313efc5549c9f0a7ab220b6f
Jiri Konecny <jkonecny@redhat.com>
2020-01-20 17:28:31 +0100

    Add basic implementation of pylint run
    Add class for configuration.
    Add class for pylint execution.
    
    Pass arguments from command line to this linter.

---
+++ censoreship/__init__.py
@@ -0,0 +1,76 @@
+# Pylint censorship -- The smart filtering of false positives for Pylint.
+#
+# Copyright (C) 2020  Red Hat, Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ["CensoreshipLinter", "CensoreshipConfig"]
+
+import sys
+
+import pylint.epylint as lint
+
+
+class CensoreshipConfig(object):
+    """Configuration of False Positives you want to run by Pylint.
+
+    Attributes:
+
+    false_possitives: List of false positives you want to filter out.
+
+    pylintrc_path: Path to the Pylint configuration file. Everything except false positives
+                   should be configured there.
+    """
+    def __init__(self):
+        self.false_positives = []
+        self.pylintrc_path = []
+
+
+class CensoreshipLinter(object):
+    """Run pylint linter and modify it's output."""
+
+    def __init__(self, config):
+        """Create CenshoreshipLinter class.
+
+        :param config: configuration class for this Linter
+        :type config: CensoreshipConfig class instance
+        """
+        self._stdout = None
+        self._stderr = None
+        self._config = config
+
+    def run(self):
+        """Run the pylint."""
+        args = self._prepare_args()
+
+        if args:
+            (self._stdout, self._sterr) = lint.py_run(command_options=args, return_std=True)
+        else:
+            (self._stdout, self._sterr) = lint.py_run(return_std=True)
+
+    def _prepare_args(self):
+        # skip first from argv it contains name of the running script
+        if len(sys.argv) == 1:
+            return ""
+
+        args = sys.argv[1:]
+
+        if self._config.pylintrc_path and "--pylintrc" not in args:
+            args.append("--pylintrc")
+            args.append(self._config.pylintrc_path)
+
+        if args:
+            args = args.join(" ")
+
+        return args
